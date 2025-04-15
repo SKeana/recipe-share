cfa7536225b0c20d27cf6452716d90e4031e83a0
SKeana <Skean2000@outlook.com>
2025-04-15 02:38:48 +0100

    fix load problem with impports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // imports
 import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import './app.css';
-import '/recipe-share-backend/server.js';
+//import axios from 'axios'
+import './App.css';
+//import '/recipe-share-backend/server.js';
 
 function App() {
   //state of recipes
